3ed51953c4765aef36b7a8dfd98b294b313036fd
ca.povedar1 <ca.povedar1@uniandes.edu.co>
2024-10-14 00:09:54 -0500

    first commit

---
+++ Readme.txt
@@ -0,0 +1,9 @@
+Author: Carlos Poveda
+Pagina web profesional sobre mi curriculum vitae moderna y responsiva que utiliza Bootstrap Icons, AOS, GLightbox y Swiper para una experiencia de usuario enriquecida.
+Tecnologías Utilizadas:
+    Bootstrap Icons
+    AOS (Animate On Scroll)
+    GLightbox
+    Swiper
+    HTML5, CSS3, JavaScript
+
---
+++ assets/js/main.js
@@ -0,0 +1,68 @@
+
+(function() {
+  
+
+  /**
+   * Preloader
+   */
+  const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove();
+    });
+  }
+
+ 
+  /**
+   * Animation on scroll function and init
+   */
+  function aosInit() {
+    AOS.init({
+      duration: 600,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', aosInit);
+
+  /**
+   * Init typed.js
+   */
+  const selectTyped = document.querySelector('.typed');
+  if (selectTyped) {
+    let typed_strings = selectTyped.getAttribute('data-typed-items');
+    typed_strings = typed_strings.split(',');
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
+
+  /**
+   * Animate the skills items on reveal
+   */
+  let skillsAnimation = document.querySelectorAll('.skills-animation');
+  skillsAnimation.forEach((item) => {
+    new Waypoint({
+      element: item,
+      offset: '80%',
+      handler: function(direction) {
+        let progress = item.querySelectorAll('.progress .progress-bar');
+        progress.forEach(el => {
+          el.style.width = el.getAttribute('aria-valuenow') + '%';
+        });
+      }
+    });
+  });
+
+ 
+
+
+
+
+ 
+})();
